a3a87dde5afc93b4bc7b86942af4bbb64bc0eb81
Jennifer <travelingmedievalist>
2019-05-11 17:08:44 -0500

    Program update
    It all works.  I'm ridiculously proud of myself

--- Final ProjectElection Data/Final Project- Election Results.py
+++ Final ProjectElection Data/Final Project- Election Results.py
@@ -4,9 +4,9 @@ import numpy as np #not sure why this wont import as I have added numpy package 
 pd.set_option('display.max_columns',8) # I wasn't seeing all my columns and I needed to in order to make sure my added columns happened
 pd.set_option('display.max_row',10) # I was getting too much that I didn't need to make sure things were working, so I limited the number displayed
 
-McH2018GE_RAW = pd.read_csv('McHENRY2018GE.csv', header= 0) #reading in my data
-McH2016GE_RAW = pd.read_csv('McHENRY2016GE.csv', header= 0)
-McH2014GE_RAW = pd.read_csv('McHENRY2014GE.csv', header= 0)
+McH2018GE_RAW = pd.read_csv('McHENRY2018GE.csv') #reading in my data
+McH2016GE_RAW = pd.read_csv('McHENRY2016GE.csv')
+McH2014GE_RAW = pd.read_csv('McHENRY2014GE.csv')
 
 def deletecolumns (df):     #cleaning up my data by deleting columns that I wasn't using
     del df['JurisdictionID']
@@ -27,22 +27,63 @@ McH2014GE_RAW["VoterTurnout"]= round((McH2014GE_RAW['VoteCount'] /McH2014GE_RAW[
 McH2016GE_RAW["VoterTurnout"]= round((McH2016GE_RAW['VoteCount'] /McH2016GE_RAW['Registration']) *100)
 McH2018GE_RAW["VoterTurnout"]= round((McH2018GE_RAW['VoteCount'] /McH2018GE_RAW['Registration']) *100)
 
-def pullprecinctdata(df, precinctname ): # function to pull out specified precinct
+McH2014GE_reordered= McH2014GE_RAW[["ContestName", "CandidateName", "PartyName", "Registration", "VoteCount", "VoterTurnout", "PrecinctName", "ElectionYear"]]
+McH2016GE_reordered= McH2016GE_RAW[["ContestName", "CandidateName", "PartyName", "Registration", "VoteCount", "VoterTurnout", "PrecinctName", "ElectionYear"]]
+McH2018GE_reordered= McH2018GE_RAW[["ContestName", "CandidateName", "PartyName", "Registration", "VoteCount", "VoterTurnout", "PrecinctName", "ElectionYear"]]
 
-    precinctdataname = df[df['PrecinctName'].str.contains(precinctname)]
-    # precinctdataname = pd.DataFrame(precinctdataname, columns = ['PrecinctName', 'ContestName', 'CandidateName', 'PartyName', 'Registration', 'VoteCount','VoterTurnout' ]
-    precinctdataname.to_csv(precinctname + precinctdataname['ElectionYear'].str + ".csv", encoding='utf-8', index=False)
+McH2018GE_cleaned= McH2018GE_reordered.dropna(subset=["ContestName", "CandidateName", "PrecinctName"])
+
+McH2014GE_Sorted = McH2014GE_reordered.sort_values(by= ["ContestName", "VoteCount"], ascending=(True, False))
+McH2016GE_Sorted = McH2016GE_reordered.sort_values(by= ["ContestName", "VoteCount"], ascending=(True, False))
+McH2018GE_Sorted = McH2018GE_cleaned.sort_values(by= ["ContestName", "VoteCount"], ascending=(True, False))
+
+
+
+
+# McH2014GE_RAW ["FileName"] = McH2014GE_RAW["PrecinctName"] + McH2014GE_RAW["ElectionYear"] + ".csv" #because Pandas is bad at allowing you to set-up a filenaming system
+# McH2016GE_RAW ["FileName"] = McH2016GE_RAW["PrecinctName"] + McH2016GE_RAW["ElectionYear"] + ".csv" #at least according to what I can find on StackOverflow and Google searching
+# McH2018GE_RAW ["FileName"] = McH2018GE_RAW["PrecinctName"] + McH2018GE_RAW["ElectionYear"] + ".csv"
+
+def pullprecinctdata14(precinctname): # function to pull out specified precinct
+    PRECINCTNAME= precinctname.upper()
+    precinctdataname = McH2014GE_Sorted[McH2014GE_Sorted['PrecinctName'].str.contains(PRECINCTNAME)]
+    fileout= precinctname + "_2014-General" + ".csv"   #I was having problems getting the file name to come out of the info in dataframe, so I opted for 3 functions instead of 1,
+    filename= fileout.lower()                         # I want to combine the 3 functions in the future
+    precinctdataname.to_csv (filename, index = 0)  # Eventually I would like to figure out how to have the data save to a subdirectory
     return
 
-pullprecinctdata(McH2014GE_RAW, "Riley")
+
+def pullprecinctdata16(precinctname):
+    PRECINCTNAME= precinctname.upper()
+    precinctdataname = McH2016GE_Sorted[McH2016GE_Sorted['PrecinctName'].str.contains(PRECINCTNAME)]
+    fileout= precinctname + "_2016-General" + ".csv"
+    filename= fileout.lower()
+    precinctdataname.to_csv (filename, index = 0)
+    return
+
+def pullprecinctdata18(precinctname):
+    PRECINCTNAME= precinctname.upper()
+    precinctdataname = McH2018GE_Sorted[McH2018GE_Sorted['PrecinctName'].str.contains(PRECINCTNAME)]
+    fileout= precinctname + "_2018-General" + ".csv"
+    filename= fileout.lower()
+    precinctdataname.to_csv (filename)
+    return
+
 
 infile = open("McHenry_County_Precinct_Names.txt", "rt", encoding = 'utf-8')
 namelist= infile.read()
 infile.close()
 
 PrecinctNameList= namelist.split('\n')
-# print(PrecinctNameList)
 
-# for precinct in PrecinctNameList:
-#     pullprecinctdata(McH2014GE_RAW, precinct)
+
+
+for precinct in PrecinctNameList:
+    pullprecinctdata14(precinct)
+    pullprecinctdata16(precinct)
+    pullprecinctdata18(precinct)
+
+
+####OMG It totally worked!!!  I am so proud of myself####
+
 
